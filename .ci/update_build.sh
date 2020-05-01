8c9fd6a4366a6079e6c74107d48d256300e9d777
亶父 <tanxpyox@gmail.com>
2020-05-02 01:19:28 +0800

    Update update_build.sh

--- .ci/update_build.sh
+++ .ci/update_build.sh
@@ -3,18 +3,16 @@ BRANCH=build
 TARGET_REPO=tanxpyox/OpenLex.git
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    echo -e "Starting to deploy to Github Pages\n"
+    echo -e "Starting to deploy to Build Branch\n"
     if [ "$TRAVIS" == "true" ]; then
         git config --global user.email "travis@travis-ci.org"
         git config --global user.name "Travis"
     fi
-    # using token clone gh-pages branch
-    git clone --quiet --branch=$BRANCH https://${GITHUB_TOKEN}@github.com/$TARGET_REPO build > /dev/null
+    # using token clone build branch
+    git clone --quiet --branch=$BRANCH https://${GITHUB_TOKEN}@github.com/$TARGET_REPO > /dev/null
     # go into directory and copy data we're interested in to that directory
-    cd build
-    cp ../readme.md ../index.html .
     # add, commit and push files
-    git add -f .
+    git add -f *.pdf
     git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to Build Branch"
     git push -fq origin $BRANCH > /dev/null
     echo -e "Deploy completed\n"
