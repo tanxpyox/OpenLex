2ee7995bc37da359e48d613b0ff8532f9dba21a8
亶父 <tanxpyox@gmail.com>
2020-05-02 01:37:48 +0800

    pass build through /build folder

--- .ci/update_build.sh
+++ .ci/update_build.sh
@@ -9,10 +9,12 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
         git config --global user.name "Travis"
     fi
     # using token clone build branch
-    git clone --quiet --branch=$BRANCH https://${GITHUB_TOKEN}@github.com/$TARGET_REPO > /dev/null
+    git clone --quiet --branch=$BRANCH https://${GITHUB_TOKEN}@github.com/$TARGET_REPO build> /dev/null
     # go into directory and copy data we're interested in to that directory
+    cd build
+    cp ../*.pdf .
     # add, commit and push files
-    git add -f *.pdf
+    git add -f .
     git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to Build Branch"
     git push -fq origin HEAD:$BRANCH > /dev/null
     echo -e "Deploy completed\n"
